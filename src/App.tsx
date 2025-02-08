57746eeea963996a43ce7733407194794d02b55f
anubhav-qt <magicalfizz@gmail.com>
2025-02-08 18:29:35 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ProposalCard from "./components/ProposalCard.tsx";
 import Memories from './components/Memories';
 import './App.css'
